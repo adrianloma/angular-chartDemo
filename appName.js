4ca03a102503406d5e7f635dae1fc26e120d8322
Adrian Lozano <adrian.loma@gmail.com>
2015-11-12 00:52:04 -0600

    Polar chart

--- appName.js
+++ appName.js
@@ -1,10 +1,6 @@
 angular.module("appName", ["chart.js"])
 .controller("nameOfController", function () {
 	var controllerScope = this;
-	controllerScope.labels =["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
-
-	controllerScope.data = [
-		[65, 59, 90, 81, 56, 55, 40],
-		[28, 48, 40, 19, 96, 27, 100]
-	];
+	controllerScope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
+	controllerScope.data = [300, 500, 100, 40, 120];
 });
