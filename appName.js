f51ad843546aa2dee4ccd1ede944f60e76bab6d5
Adrian Lozano <adrian.loma@gmail.com>
2015-11-12 00:46:48 -0600

    Line chart

--- appName.js
+++ appName.js
@@ -1,6 +1,14 @@
 angular.module("appName", ["chart.js"])
 .controller("nameOfController", function () {
 	var controllerScope = this;
-  controllerScope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  controllerScope.data = [300, 500, 100];
+
+  controllerScope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  controllerScope.series = ['Series A', 'Series B'];
+  controllerScope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  controllerScope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
 });
