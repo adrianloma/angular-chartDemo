1ef2070d8d73744ea1d1aadf100564d8af1b2f82
Adrian Lozano <adrian.loma@gmail.com>
2015-11-12 00:41:44 -0600

    Doughnut chart

--- appName.js
+++ appName.js
@@ -1,11 +1,6 @@
 angular.module("appName", ["chart.js"])
-	.controller("nameOfController", function () {
-		var controllerScope = this;
-	    controllerScope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
-	    controllerScope.data = [300, 500, 100, 40, 120];
-	    controllerScope.type = 'PolarArea';
-
-	    controllerScope.toggle = function () {
-	  		controllerScope.type = controllerScope.type === 'PolarArea' ? 'Pie' : 'PolarArea';
-	    };
-	});
+.controller("nameOfController", function () {
+	var controllerScope = this;
+  controllerScope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
+  controllerScope.data = [300, 500, 100];
+});
