5269a27518dfca6ba92178aa29e855165c7b352f
Adrian Lozano <adrian.loma@gmail.com>
2015-11-12 00:49:13 -0600

    Radar chart

--- appName.js
+++ appName.js
@@ -1,14 +1,10 @@
 angular.module("appName", ["chart.js"])
 .controller("nameOfController", function () {
 	var controllerScope = this;
+	controllerScope.labels =["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
 
-  controllerScope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  controllerScope.series = ['Series A', 'Series B'];
-  controllerScope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  controllerScope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
+	controllerScope.data = [
+		[65, 59, 90, 81, 56, 55, 40],
+		[28, 48, 40, 19, 96, 27, 100]
+	];
 });
