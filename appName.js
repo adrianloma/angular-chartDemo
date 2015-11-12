bcf76a07dc0e0c05958a5db18fed5ba8ce00db1d
Adrian Lozano <adrian.loma@gmail.com>
2015-11-12 00:38:27 -0600

    Dynamic chart

--- appName.js
+++ appName.js
@@ -1,14 +1,11 @@
 angular.module("appName", ["chart.js"])
-.controller("nameOfController", function () {
-	var controllerScope = this;
+	.controller("nameOfController", function () {
+		var controllerScope = this;
+	    controllerScope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
+	    controllerScope.data = [300, 500, 100, 40, 120];
+	    controllerScope.type = 'PolarArea';
 
-	controllerScope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-
-	controllerScope.series = ['Series A', 'Series B'];
-
-	controllerScope.data = [
-		[65, 59, 80, 81, 56, 55, 40],
-		[28, 48, 40, 19, 86, 27, 90]
-	];
-
-});
+	    controllerScope.toggle = function () {
+	  		controllerScope.type = controllerScope.type === 'PolarArea' ? 'Pie' : 'PolarArea';
+	    };
+	});
