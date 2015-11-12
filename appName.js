68006cc76bc698bdd6ba51ec1fc5e76b6dfd79dd
Adrian Lozano <adrian.loma@gmail.com>
2015-11-12 00:19:02 -0600

    Bar Chart

--- appName.js
+++ appName.js
@@ -1,8 +1,14 @@
 angular.module("appName", ["chart.js"])
-	.controller("nameOfController", function() {
-		var controllerScope = this;
+.controller("nameOfController", function () {
+	var controllerScope = this;
 
-		controllerScope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-		controllerScope.data = [300, 500, 100];
+	controllerScope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
 
-	});
+	controllerScope.series = ['Series A', 'Series B'];
+
+	controllerScope.data = [
+		[65, 59, 80, 81, 56, 55, 40],
+		[28, 48, 40, 19, 86, 27, 90]
+	];
+
+});
