96cd1212fb486151b00920a3cdee99c99673591b
Adrian Lozano <adrian.loma@gmail.com>
2015-11-12 02:03:17 -0600

    Reactive and dynamic charts demo

--- appName.js
+++ appName.js
@@ -2,26 +2,23 @@ angular.module("appName", ["chart.js"])
 .controller("nameOfController", function () {
 	var controllerScope = this;
 
-	controllerScope.labels =["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"];
+    controllerScope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"];
+    controllerScope.data = [300, 500, 100, 40, 120];
 
-	controllerScope.data = [
-	 	[100,100,100,100,100,100,100],
-	 	[100,100,100,100,100,100,100]
-	];
+	var currType = 0;
 
-	var n = 0;
+	var typeArray = ['PolarArea', 'Pie', 'Doughnut'];
+
+	controllerScope.type = 'PolarArea';
+    controllerScope.toggle = function () {
+		currType = (currType + 1) % typeArray.length;
+		controllerScope.type = typeArray[currType];
+     };
+
 
 	controllerScope.randomize = function () {
-		controllerScope.data.forEach(randFunction, n);
-		n = 0;
-	}
-
-	function randFunction(input, n) {
-		var i;
-		var arr = [];
-		 for(i = 0; i < input.length; i++) {
-			arr[i]  = 100 * Math.random()
+		for(i = 0; i < controllerScope.data.length; i++) {
+			controllerScope.data[i]  = 100 * Math.random()
 		 }
-		controllerScope.data[n++] = arr;
 	}
 });
